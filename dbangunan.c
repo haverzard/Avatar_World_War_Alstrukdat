61cd011e2686f571a33c730d42bef8a6957e20f6
V <48679462+haverzard@users.noreply.github.com>
2019-11-23 23:27:28 +0700

    Added driver bangunan, mesinkar, pcolor

---
+++ dbangunan.c
@@ -0,0 +1,62 @@
+/* Driver Bangunan */
+
+#include <stdio.h>
+#include "bangunan.h"
+#include "point.h"
+
+int main() {
+	/* Kamus */
+	Bangunan B1, B2, B3;
+	POINT temp;
+
+	/* Inisiasi */
+	Jenis(BangunanUndef) = JenisUndef;
+	temp = MakePOINT(5,5);
+
+	/* Algoritma */
+	/* Test Init & Print */
+	InitBangunan(&B1,'C');
+	InitBangunan(&B2,'F');
+	InitBangunan(&B3,'T');
+	Koordinat(B1) = temp;
+	Koordinat(B2) = temp;
+	Koordinat(B3) = temp;
+	printf("Bangunan B1: "); PrintInfoBangunan(B1);
+	printf("Bangunan B2: "); PrintInfoBangunan(B2);
+	printf("Bangunan B3: "); PrintInfoBangunan(B3);
+	
+	/* Test Valid */
+	printf("Validasi B1: %d\n", IsBangunanValid(B1));
+	printf("Validasi BangunanUndef: %d\n", IsBangunanValid(BangunanUndef));
+	
+	/* Test EQ */
+	printf("EQ B1-B1: %d\n", EQ_Bangunan(B1, B1));
+	printf("EQ B1-B2: %d\n", EQ_Bangunan(B1, B2));
+
+	/* Test Move */
+	Move(&B2, &B3, 10);
+	printf("Pindah 10 pasukan dari B2 ke B3\n");
+	printf("Bangunan B2: "); PrintInfoBangunan(B2);
+	printf("Bangunan B3: "); PrintInfoBangunan(B3);
+
+	/* Test Serang */
+	SerangBangunan(&B3, &B2, 8);
+	printf("Serang B2 dengan 8 pasukan dari B3\n");
+	printf("Bangunan B2: "); PrintInfoBangunan(B2);
+	printf("Bangunan B3: "); PrintInfoBangunan(B3);
+	SerangCritical(&B2, &B3, 40);
+	printf("Serang B3 dengan 40 pasukan dari B2\n");
+	printf("Bangunan B2: "); PrintInfoBangunan(B2); 
+	printf("Bangunan B3: "); PrintInfoBangunan(B3);
+
+	/* Test Update Bangunan */
+	UpdateBangunan(&B1);
+	UpdateBangunan(&B2);
+	printf("Bangunan B1: "); PrintInfoBangunan(B1);
+	printf("Bangunan B2: "); PrintInfoBangunan(B2);
+
+	/* Test Level Up */
+	LevelUpBangunan(&B1);
+	printf("Bangunan B1: "); PrintInfoBangunan(B1);
+
+}
---
+++ dpcolor.c
@@ -0,0 +1,19 @@
+/* Driver Pcolor */
+
+#include <stdio.h>
+#include "pcolor.h"
+
+int main() {
+	/* Algoritma */
+	/* Test warna */
+	print_red('b');
+	print_green('e');
+	print_yellow('z');
+	print_blue('a');
+	print_magenta('l');
+	print_cyan('e');
+
+	/* Test printByColorNum */
+	printByColorNum(1,'l');
+	printf("\n");
+}
